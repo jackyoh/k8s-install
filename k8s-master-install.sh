6a587487a09dcd0e4f511efdc35173072d826486
jack <jack@is-land.com.tw>
2022-12-11 21:47:30 +0800

    Set kubernetes version

--- k8s-master-install.sh
+++ k8s-master-install.sh
@@ -5,7 +5,7 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet=1.25.5-00 kubeadm=1.25.5-00 kubectl=1.25.5-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo systemctl start kubelet
